025223f7d632876b0981e57d4b4248516171a171
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-14 09:45:55 -0700

    functional settings page

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,57 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget {
-   
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
   static const String routeName = 'Settings';
-  
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode = false;
+  int gender = 1;
+  String name = 'Aydin';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      drawer: const SideMenu(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            Text('Settings', style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300)),
-            Divider(),
-            SwitchListTile.adaptive(
-              title: const Text('Dark Mode'),
-              value: true, 
-              onChanged: (value){}),
-            Divider(),
-            RadioListTile<int>(
-              title: Text('Masculino'),
-              value: 1,
-              groupValue: 1,
-              onChanged: (value) {},
-            ),
-            Divider(),
-            RadioListTile<int>(
-              title: Text('Masculino'),
-              value: 2,
-              groupValue: 1,
-              onChanged: (value) {},
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextFormField(
-                initialValue: 'Aydin',
-                decoration: InputDecoration(
-                  labelText: 'Nombre',
-                  helperText: 'Nombre del usuario'
-                ),
-                onChanged: (value) {},
+        appBar: AppBar(title: const Text('Settings')),
+        drawer: const SideMenu(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Text('Settings',
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300)),
+              const Divider(),
+              SwitchListTile.adaptive(
+                  title: const Text('Dark Mode'),
+                  value: isDarkMode,
+                  onChanged: (value) {
+                    isDarkMode = value;
+                    setState(() {});
+                  }),
+              const Divider(),
+              RadioListTile<int>(
+                title: const Text('Masculino'),
+                value: 1,
+                groupValue: gender,
+                onChanged: (value) {
+                  gender = value ?? 1;
+                },
               ),
-            ),
-          ]),),
-      )
-    );
+              const Divider(),
+              RadioListTile<int>(
+                title: const Text('Femenino'),
+                value: 2,
+                groupValue: gender,
+                onChanged: (value) {
+                  gender = value ?? 2;
+                },
+              ),
+              const Divider(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextFormField(
+                  initialValue: name,
+                  decoration: const InputDecoration(
+                      labelText: 'Nombre', helperText: 'Nombre del usuario'),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                ),
+              ),
+            ]),
+          ),
+        ));
   }
 }
