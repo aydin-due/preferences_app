73f11721efd2f6fe1674c00306b5a6709e38cfce
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-13 23:34:08 -0700

    drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
    
@@ -9,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
+      drawer: SideMenu(),
       body: const Center(
          child: Text('HomeScreen'),
       ),
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class SettingsScreen extends StatelessWidget {
    
@@ -10,6 +11,7 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
+      drawer: const SideMenu(),
       body: const Center(
          child: Text('SettingsScreen'),
       ),
