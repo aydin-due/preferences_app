2262be34d277280b2103679ec1c5a5b555442ea7
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-14 09:38:16 -0700

    screens design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
   static const String routeName = 'Home';
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       drawer: SideMenu(),
-      body: const Center(
-         child: Text('HomeScreen'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('isDarkMode: '), 
+          Divider(), 
+          Text('Género:'), 
+          Divider(), 
+          Text('Nombre:')],
       ),
     );
   }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -12,9 +12,46 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
       drawer: const SideMenu(),
-      body: const Center(
-         child: Text('SettingsScreen'),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text('Settings', style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300)),
+            Divider(),
+            SwitchListTile.adaptive(
+              title: const Text('Dark Mode'),
+              value: true, 
+              onChanged: (value){}),
+            Divider(),
+            RadioListTile<int>(
+              title: Text('Masculino'),
+              value: 1,
+              groupValue: 1,
+              onChanged: (value) {},
+            ),
+            Divider(),
+            RadioListTile<int>(
+              title: Text('Masculino'),
+              value: 2,
+              groupValue: 1,
+              onChanged: (value) {},
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextFormField(
+                initialValue: 'Aydin',
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  helperText: 'Nombre del usuario'
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+          ]),),
+      )
     );
   }
 }
