b8ca3d5e3faf8f08d67b0a57bb5660e7ff878092
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-13 21:48:37 -0700

    set up screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        SettingsScreen.routeName: (context) => const SettingsScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+   
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  static const String routeName = 'Settings';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: const Center(
+         child: Text('SettingsScreen'),
+      ),
+    );
+  }
+}
